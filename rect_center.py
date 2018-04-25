abbac1bb94fd1e414d412725787fe0a80fa32504
gitthb <38727696+gitthb@users.noreply.github.com>
2018-04-26 00:09:20 +0800

    Add files via upload

---
+++ rect_center.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 26 00:04:44 2018
+
+@author: Administrator
+用于测试从图像中获取指定矩形区域的中心点坐标
+"""
+import numpy as np
+import cv2
+import sys
+import os
+if __name__ == "__main__":
+    print("hello world")
